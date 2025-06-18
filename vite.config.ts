77945038a9dbbccdc45a709a440bf56bb3b1008d
Kiana Dimitri <maisan098704@gmail.com>
2025-06-18 11:47:24 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'path';
 
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,16 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-import path from 'node:path'
-import url from 'node:url'
-
-const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), flowbiteReact()],
-   resolve: {
-    alias: {
-      "@shared": resolve(_dirname, "./shared"),
-    }
-  },
 });
